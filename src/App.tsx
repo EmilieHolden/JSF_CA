8548397349488009517920988fbc3075cfefcb0e
EmilieHolden <e.393@hotmail.com>
2026-09-25 12:09:12 +0200

    set up feature for application routing

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+import HomeView from "./views/HomeView"
+import ProductView from "./views/ProductView"
+import CartView from "./views/CartView"
+import ContactView from "./views/ContactView"
+import CheckoutSuccessView from "./views/CheckoutSuccessView"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+          <Route path="/product/:id" element={<ProductView />} />
+          <Route path="/cart" element={<CartView />} />
+          <Route path="/contact" element={<ContactView />} />
+          <Route path="/checkout-success" element={<CheckoutSuccessView />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
