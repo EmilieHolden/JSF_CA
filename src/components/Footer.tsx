a151b869e6e5ab3091e3a6c7d11fb58573da781a
EmilieHolden <e.393@hotmail.com>
2026-09-25 12:07:06 +0200

    implemented footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="bg-beige p-4 text-text w-full">
+      <p>Altora</p>
+      <p>© 2026 Emilie Holden</p>
+    </footer>
+  )
+}
+
+export default Footer
